4f704d8ccc844873eee30e9f9fd2a076c786df49
Ricardo Bocchi <ricardo@mobilemind.com.br>
2023-12-14 09:21:48 -0300

    first publication

---
+++ android/app/src/main/kotlin/com/example/cljd_amigo_secreto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cljd_amigo_secreto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,1 @@
+export "cljd-out/br/com/mobilemind/main.dart" show main;
